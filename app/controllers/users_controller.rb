dae0298b665b7c92cb0c7dea879a18b2b5f95bb3
ToIshikawa <welovegabba2.happycore@gmail.com>
2019-06-25 21:09:51 +0900

    create new-member-registration
    users_controllerを作成
    _footer.html.haml
    _header.html.haml
    new_registration.html.haml
    とそれぞれのscssを作成
    new_registrationを呼び出すために
    ルーティングを追記

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def new_registration
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products
   resources :mypages
+  get   'users'  => 'users#new_registration'
 end
