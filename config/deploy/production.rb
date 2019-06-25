be093e4089b394b0e797ca9ab95cd075eb758c63
masa79 <ngsh.sesh@gmail.com>
2019-06-25 18:58:37 +0900

    Edit_production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '54.249.3.233', user: 'ec2-user', roles: %w{app db web}
+
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -59,3 +59,4 @@ server '54.249.3.233', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '54.249.3.233', user: 'ec2-user', roles: %w{app db web}
