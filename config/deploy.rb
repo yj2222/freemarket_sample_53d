e773cb05eae4b8af0c0ed1f2559cca67bd43fd75
masa79 <ngsh.sesh@gmail.com>
2019-06-25 19:14:09 +0900

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "freemarket_sample_53d"
-set :repo_url, "git@github.com:masa79/freemarket_sample_53d.git"
+set :repo_url, "git@github.com:yj2222/freemarket_sample_53d.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
