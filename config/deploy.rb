b1edb72c07fa79ea4f6fd1b8316c9839232db4d7
masa79 <ngsh.sesh@gmail.com>
2019-06-26 20:12:40 +0900

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
+set :format_options, truncate: false
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/f10157.pem']
