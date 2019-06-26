cfb1bd94c1ecdc2353992c0c4301c3d3f196eba2
yuji masujima <masjima2712@gmail.com>
2019-06-26 14:28:42 +0900

    Create mypage files
    本人確認ページ、ログアウトのビュー実装。
    また作業に伴いコントローラや不要箇所の削除などもした。

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -5,4 +5,19 @@ class MypagesController < ApplicationController
   def edit
   end
   
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
