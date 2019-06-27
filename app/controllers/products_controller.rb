61edc60cdf3d2923317f2abd49eeb903b20f66f9
limasolTK <takana1997@gmail.com>
2019-06-27 11:40:39 +0900

    initail commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,23 @@
 class ProductsController < ApplicationController
   def index
-  end 
-
-  def show 
   end
+
+  def edit
+  end
+  
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
