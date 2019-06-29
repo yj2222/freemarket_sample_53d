b337a701df2870753dd237e6b4636f1d0c25ca45
yuji masujima <masjima2712@gmail.com>
2019-06-29 14:03:03 +0900

    Update

--- config/20190628135042_add_references_trade.rb
+++
@@ -1,7 +0,0 @@
-class AddReferencesTrade < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :trades, :user, foreign_key: true, null: false, index: true
-    add_reference :trades, :product, foreign_key: true, null: false, index: true
-    # add_reference :trades, :seller, foreign_key: true, null: false, index: true
-  end
-end
--- db/migrate/20190628134915_add_references_to_like.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToLike < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :likes, :user, foreign_key: true, null: false, index: true
-    add_reference :likes, :product, foreign_key: true, null: false, index: true
-  end
-end
