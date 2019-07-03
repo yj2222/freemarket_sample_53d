c7c0bcc5e6ec93868e9bac010b26ac8764db03b4
masa79 <ngsh.sesh@gmail.com>
2019-07-03 17:59:00 +0900

    Update purchase.html.haml

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 2019_07_02_111024) do
     t.string "parent", null: false
     t.string "child"
     t.string "son"
+    t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_categories_on_product_id"
   end
 
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -67,17 +69,16 @@ ActiveRecord::Schema.define(version: 2019_07_02_111024) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "size"
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "size", null: false
     t.string "brand"
-    t.string "delivery_price"
-    t.string "delivery_type"
-    t.string "prefecture"
-    t.string "delively_days"
-    t.integer "price"
-    t.integer "condition"
-    t.bigint "user_id"
+    t.string "delibary_price", null: false
+    t.string "delibary_type", null: false
+    t.string "prefecture", null: false
+    t.string "delivaly_days", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
@@ -90,7 +91,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_111024) do
     t.string "lastname_kanji", null: false
     t.string "firstname_katakana", null: false
     t.string "lastname_katakana", null: false
-    t.string "birth_year", null: false
+    t.string "birth_yaer", null: false
     t.string "birth_month", null: false
     t.string "birth_day", null: false
     t.integer "phone_number"
@@ -126,6 +127,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_111024) do
   end
 
   add_foreign_key "areas", "users"
+  add_foreign_key "categories", "products"
   add_foreign_key "credits", "users"
   add_foreign_key "images", "products"
   add_foreign_key "likes", "products"
