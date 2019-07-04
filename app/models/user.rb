22f7279db24fddd8ae776e4766704132ccb8bc5b
ToIshikawa <welovegabba2.happycore@gmail.com>
2019-07-04 13:32:26 +0900

    モデルにアソシエーションを指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :likes, dependent: :destroy
+  has_one :credit, dependent: :destroy
+  has_one :profile, dependent: :destroy
+  has_one :area, dependent: :destroy
+  accepts_nested_attributes_for :profile
+  accepts_nested_attributes_for :area
 end
