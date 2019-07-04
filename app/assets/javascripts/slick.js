9061001c5ad432bebb65ba7d8958710352976c7f
t-sugiyama-tech <new.old.2611@gmail.com>
2019-07-04 18:22:00 +0900

    display_item_outline

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,6 +1,8 @@
+// 実装の最中です。こちらのファイルは残していただける助かります。
+
 // $(function(){
 //   $("#slider").slick({
 //     prevArrow: '<img src="https://www-mercari-jp.akamaized.net/assets/img/featured/camp/index/index_keyvisual.png?2921618501" class="slide-arrow prev-arrow">',
 //     nextArrow: '<img src="https://www-mercari-jp.akamaized.net/assets/img/common/jp/top/main_content_pc.png?2921618501" class="slide-arrow next-arrow">'
 //   });
-// });
+// }); 
