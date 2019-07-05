ce3888d9c0d10c897b1d1e99945f606c3ab473ba
masa79 <ngsh.sesh@gmail.com>
2019-07-05 17:12:26 +0900

    Edit migration file

---
+++ db/migrate/20190705061209_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :size
+      t.references :brand, index: true, foreign_key: true
+      t.string :delivery_price
+      t.string :delivery_type
+      t.string :prefecture
+      t.string :delivery_days
+      t.integer :price
+      t.integer :condition
+      t.references :user, index: true, foreign_key: true
+      t.timestamps
+    end
+  end
+end
