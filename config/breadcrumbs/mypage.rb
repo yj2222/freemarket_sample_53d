60db4aeed5056f83c818180dc0e703d901a90abe
t-sugiyama-tech <new.old.2611@gmail.com>
2019-07-09 00:37:42 +0900

    fix_func_hansel_gretel

--- config/breadcrumbs/mypage.rb
+++ config/breadcrumbs/mypage.rb
@@ -20,30 +20,4 @@ end
 crumb :logout do 
   link "ログアウト", logout_mypages_path 
   parent :mypages
-end 
-
-
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+end
