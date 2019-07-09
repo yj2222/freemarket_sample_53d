5818c209644b5165bda4442821b1a3a5e663c07e
yuji masujima <masjima2712@gmail.com>
2019-07-09 16:22:07 +0900

    Update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,6 +37,7 @@ class ProductsController < ApplicationController
   end
 
   def create
+    binding.pry
     product = Product.new(params_int(product_params))
     if product.save
       
@@ -72,7 +73,7 @@ class ProductsController < ApplicationController
 
   def image_params(num)
       last_id = Product.pluck(:id).last
-      params.require(:images).require("#{num}").permit(:image_url).merge(product_id: last_id)
+      params.require(:images).require("#{num}").permit(:url).merge(product_id: last_id)
   end
 
   # 送られたパラメータを整数化する。
