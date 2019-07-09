ca84858f604b7786cc17ffb6295e78c5dad84406
limasolTK <takana1997@gmail.com>
2019-07-09 15:10:40 +0900

    出品者のその他の商品実装による細かい修正

--- db/migrate/20190706055338_create_images.rb
+++ db/migrate/20190706055338_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :image_url, null: false
+      t.string :url, null: false
       t.references :product, null: false, index: true, foreign_key: true
       t.timestamps
     end
