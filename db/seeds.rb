20ca9af7776de8b792236064cac0ec7295631c06
yuji masujima <masjima2712@gmail.com>
2019-07-09 14:10:30 +0900

    Create seeds.rb
    シードファイルとCSVを作成。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/categories.csv', headers: true) do |row|
+  Category.create(
+    id: row['id'],
+    grandchild: row['grandchild'],
+    child: row['child'],
+    parent: row['parent'],
+    created_at: row['created_at'],
+    updated_at: row['updated_at']
+  )
+end
