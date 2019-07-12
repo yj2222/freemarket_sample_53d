c54508e94cce78bec53a6dd622c6789d66ab09ee
ToIshikawa <welovegabba2.happycore@gmail.com>
2019-07-12 16:56:03 +0900

    create omniauth_callbacks_controller__action & create model
    user.rbでSNS側から送られてきた値を取得して変数に代入
    コールバック用にコントローラのアクションを作成
    コールバックした際に、モデルで指定した変数が入ったインスタンスを作成し、サインイン状態にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable
   has_one :profile
   has_many :like
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.nickname = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 
   has_many :products
   has_many :likes, dependent: :destroy
