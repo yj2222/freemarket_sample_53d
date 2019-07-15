0e4b2f784e1d61c73ea7933bedfa98e7accf475f
ToIshikawa <welovegabba2.happycore@gmail.com>
2019-07-15 13:02:29 +0900

    refactoring routes.rb & views
    不必要なルーティング、ビューを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'users/sessions#signup'
-    get 'signup/information', to: 'users/registrations#information'
-    get 'signup/tel_authentication', to: 'users/registrations#tel_authentication'
-    get 'signup/address', to: 'users/registrations#address'
+    get 'signup/all', to: 'users/registrations#all'
+    post 'signup/all', to: 'users/registrations#create'
     get 'signup/payment', to: 'users/registrations#payment'
     post 'signup/payment', to: 'users/registrations#create_payment'
     get 'signup/complete', to: 'users/registrations#complete'
-    post 'users', to: 'users/registrations#create'
-    get 'signup/all', to: 'users/registrations#all'
-    post 'signup/all', to: 'users/registrations#create'
     delete 'mypages/logout', to: 'users/sessions#destroy'
   end
 
