225a5e2ee3c1e2bdfc49bca0f5de46bc8a20f47a
ToIshikawa <welovegabba2.happycore@gmail.com>
2019-07-17 14:29:46 +0900

    fix render payment_path (and others)
    ユーザーの新規登録後、クレジット登録画面に飛ばす方法をrenderからredirect_toに変更
    フラッシュメッセージを削除
    不要なメソッドを削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,7 +15,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.present?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.omniauth_data"] = request.env["omniauth.auth"]
       redirect_to signup_all_path
