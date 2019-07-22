130e4b61212f48e9396cb0193e0d635fb6be9db0
limasolTK <takana1997@gmail.com>
2019-07-22 16:25:14 +0900

    未ログイン時のトップページ飛ばしを実装しました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:edit,:show,:update,:purchase,:exhibit]
+  before_action :check_signin, only: [:show,:purchase,:exhibit]
 
   def index  
     # カテゴリーの一覧 スコープを使用 model/product.rb参照
@@ -153,6 +154,10 @@ class ProductsController < ApplicationController
     true
   rescue ArgumentError
     false
+  end
+
+  def check_signin
+    redirect_to root_path unless user_signed_in?
   end
 
   def params_int(product_params)
